faec2930a2da8abdaa6685b225be4e1a8b5e8b98
Dr-0000FF <dr.000ffbiz@gmail.com>
2025-07-11 16:17:14 -0500

    Created Startup transition

--- ETC/notes.md
+++ ETC/notes.md
@@ -1,27 +1,27 @@
-Space for the hero image 
+- [ ] Space for the hero image 
 
-Collapsable nav bar 
+- [ ] Collapsable nav bar 
     Long oval shape 
 
-Under the nav button place "Lets Conncenct" Or something to take user to about form
+- [ ] Under the nav button place "Lets Conncenct" Or something to take user to about form
 
-Place linkdin and GitHub unerd contact 
+- [ ] Place linkdin and GitHub unerd contact 
 
 
 *PROJECTS*
-Homepage should scroll down to gradient background where Projects are shown with a quick snippet of what the projects are
+- [ ] Homepage should scroll down to gradient background where Projects are shown with a quick snippet of what the projects  are
 
-Going to the Projects in home takes user to Project page where a list of some sort is there with tech stacks and more in depth information is about the project 
+- [ ] Going to the Projects in home takes user to Project page where a list of some sort is there with tech stacks and more in depth information is about the project 
 
-link to the Github project 
+- [ ] link to the Github project 
 
 *About* 
-Have a light and dark section with Dr.0000FF being one option, and moses being another. 
+- [ ] Have a light and dark section with Dr.0000FF being one option, and moses being another. 
 
-Give information on both entities 
+- [ ] Give information on both entities 
 
 *Contact*
-Give business number and email as well as personal email 
+- [ ] Give business number and email as well as personal email 
 
 
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './index.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [slideUp, setSlideUp] = useState(false)
+  const [slideDn, setSlideDn] = useState(false)
+
+  // Slide black up after 500ms
+  useEffect(() => {
+    const timer = setTimeout(() => setSlideUp(true), 500)
+    return () => clearTimeout(timer)
+  }, [])
+
+  // Slide white down after 1500ms
+  useEffect(() => {
+    const timer = setTimeout(() => setSlideDn(true), 1000)
+    return () => clearTimeout(timer)
+  }, [])
 
   return (
-    <div className='w-full h-full absolute
-                  bg-[#0000FF] 
-                    bg-[linear-gradient(123deg,_rgba(0,_0,_255,_1)_0%,_rgba(27,_152,_224,_1)_31%,_rgba(189,_237,_224,_1)_74%)]'>
-      {/*Navigation Bar*/}
-      <header className='flex justify-between items-center'>
+    <div className="relative w-full h-screen overflow-hidden">
+      {/* Black Overlay */}
+      <div
+        className={`w-full h-full absolute top-0 left-0 z-20 bg-black transition-transform duration-[500ms] ease-in-out
+        ${slideUp ? "-translate-y-full" : "translate-y-0"}`}
+      />
 
-      </header>
+      {/* White Overlay */}
+      <div
+        className={`w-full h-full absolute top-0 left-0 z-10 bg-white transition-transform duration-[1000ms] ease-in-out
+        ${slideDn ? "translate-y-full" : "translate-y-0"}`}
+      />
 
-      {/* Hero Section */}
-
-      {/* Main Content */}
-
-      {/* Footer */}
-
+      {/* Gradient Background */}
+      <div className="w-full h-full absolute top-0 left-0 z-0
+        bg-[linear-gradient(123deg,_rgba(0,_0,_255,_1)_0%,_rgba(27,_152,_224,_1)_31%,_rgba(189,_237,_224,_1)_74%)]">
+        {/* Site content goes here */}
+      </div>
     </div>
   )
 }
