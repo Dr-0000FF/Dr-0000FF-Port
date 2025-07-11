c87768b9baa5df18e370e5f8c3ef52eba3d2c0f9
Dr-0000FF <dr.000ffbiz@gmail.com>
2025-07-11 01:54:03 -0500

    Installing react and Tailwind creating bare-bones

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      {/* Hero Section */}
+
+      {/*Navigation Bar*/}
+
+      {/* Main Content */}
+
+      {/* Footer */}
+
+    </div>
+  )
+}
+
+export default App
